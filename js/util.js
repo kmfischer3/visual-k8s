091055e7b8c178cbd4cd147f0af5a040c7042a5c
kristina <kristina@martian>
2017-11-05 07:44:00 -0600

    adds fail flash animation

--- js/util.js
+++ js/util.js
@@ -1,1 +1,12 @@
-console.log("hello");
+function killNode(node_id) {
+  if (!node_id)
+    return;
+
+  // -webkit-animation: flash-dead 1s infinite;
+  // -moz-animation: flash-dead 1s infinite;
+  // -o-animation: flash-dead 1s infinite;
+  // animation: flash-dead 1s infinite;
+
+  document.getElementById(node_id).style.animation = "flash-dead 1s infinite";
+  return;
+}
